7bf18ab1a3b9bd2681dc7004214bda7b53c91e06
kiddingstick <satypanday@gmail.com>
2025-08-22 23:06:11 +0530

    updates

--- rmi/routes/auth.js
+++ rmi/routes/auth.js
@@ -1,5 +1,5 @@
 import express from 'express';
-//import bcrypt from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
 import PendingUser from '../models/pendingUser.js';
